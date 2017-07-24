1aae8669f6636bc2145047e58db1073ab99856cd
Sneha K <srkrish2@illinois.edu>
2017-07-24 13:17:36 -0400

    login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { AuthService } from '../../providers/auth-service';
+import { AuthService } from '../../providers/auth-service/auth-service';
  
 @Component({
   selector: 'page-home',
