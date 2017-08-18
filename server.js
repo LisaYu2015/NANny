623b4676b796e33feef540417b33063e9ca44a72
Sneha K <srkrish2@illinois.edu>
2017-08-18 13:52:15 -0400

    testing

--- server.js
+++ server.js
@@ -65,10 +65,10 @@ var assert = require('assert');
 console.log("Anyone here?");
 var local = 'mongodb://localhost:27017/testdb';
 var url2 = 'mongodb://website:Bosch1234567@ec2-54-87-140-197.compute-1.amazonaws.com:27017/testdb';
-mongoose.connect(local); 
+mongoose.connect(url2); 
 // When successfully connected
 mongoose.connection.on('connected', function () {  
-  console.log('Mongoose default connection open to ' + local);
+  console.log('Mongoose default connection open to ' + url2);
 }); 
 // If the connection throws an error
 mongoose.connection.on('error',function (err) {  
